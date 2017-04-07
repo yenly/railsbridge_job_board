f0a0bf992d4393b1988a3fac5d1d12fbbd50effb
Yenly Ma <yencodes@gmail.com>
2017-04-06 18:04:32 -0700

    Add update and delete options to job listing.

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -9,6 +9,19 @@ class JobsController < ApplicationController
     Job.create(job_params)
     redirect_to jobs_path
   end
+  def edit
+    @job = Job.find(params[:id])
+  end
+  def update
+    @job = Job.find(params[:id])
+    @job.update_attributes(job_params)
+    redirect_to jobs_path
+  end
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    redirect_to jobs_path
+  end
 
   private
 
